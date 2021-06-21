954301d6dd87be192561634dbf799222b9531c7e
Jeff Pottruff <jeffpottruff@gmail.com>
2021-06-21 13:07:16 -0400

    replace lorem

--- pages/about.js
+++ pages/about.js
@@ -8,10 +8,14 @@ export default function about() {
         <h3 className="text-2xl mb-5">Simple Recipes</h3>
 
         <p className="mb-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-          perferendis corporis dignissimos neque provident! Excepturi voluptatum
-          tempora totam impedit explicabo obcaecati officiis magnam officia
-          aliquid. A dolorem dicta iusto qui.
+          Wrestling is a fraternity, and the boys will work their butts off for
+          you as long as you respect them and don't lie. Cup o' coffee in the
+          big time, yeah. And there is no-one that does it better than the Macho
+          Man Randy Savage! I hate you, I hate your guts! And that's what's
+          going to be left all over the mat. Yeah what were you doing at
+          Wrestlemania? Comparatively speaking, to the Macho Man Randy Savage,
+          you're nothing but garbage! I feel so fortunate that I had a second
+          chance to marry my first love, here where it all began.
         </p>
 
         <p>
