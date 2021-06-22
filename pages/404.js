2e2dbe80ef61d1d0007903eeb5943151cc522641
Jeff Pottruff <jeffpottruff@gmail.com>
2021-06-22 07:55:18 -0400

    fix some naming

--- pages/404.js
+++ pages/404.js
@@ -5,15 +5,17 @@ import Layout from '../components/Layout';
 export default function NotFoundPage() {
   return (
     <Layout title="Page Not Found">
-      <div class="flex flex-col items-center mt-20">
+      <div className="flex flex-col items-center mt-20">
         <Image
           src="/images/logo.png"
           width={70}
           height={70}
           className="bg-gray-800 rounded-2xl"
         />
-        <h1 class="text-6xl my-5">Whoops!</h1>
-        <h2 class="text-4xl text-gray-400 mb-5">This page does not exist</h2>
+        <h1 className="text-6xl my-5">Whoops!</h1>
+        <h2 className="text-4xl text-gray-400 mb-5">
+          This page does not exist
+        </h2>
       </div>
     </Layout>
   );
