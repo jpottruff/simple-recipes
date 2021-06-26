af1baf6ad1c8fe87a7c109da32388a8a1b20c011
Jeff Pottruff <jeffpottruff@gmail.com>
2021-06-26 12:42:02 -0400

    add recipe card component

---
+++ components/Recipe.js
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Recipe({ recipe }) {
+  return (
+    <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+      <Image
+        src={recipe.frontmatter.cover_image}
+        alt=""
+        height={420}
+        width={600}
+        className="mb-4 rounded"
+      />
+      <div className="flex justify-between items-center">
+        <span className="font-light text-gray-600">
+          {recipe.frontmatter.date}
+        </span>
+        <div>{recipe.frontmatter.category}</div>
+      </div>
+
+      <div className="mt-2">
+        <Link href={`/blog/${recipe.slug}`}>
+          <a className="text-2xl text-gray-700 font-bold hover:underline">
+            {recipe.frontmatter.title}
+          </a>
+        </Link>
+        <p className="mt-2 text-gray-600">{recipe.frontmatter.excerpt}</p>
+      </div>
+
+      <div className="flex justify-between items-center mt-6">
+        <Link href={`/blog/${recipe.slug}`}>
+          <a className="text-gray-900 hover:text-blue-600">Let's do it!</a>
+        </Link>
+        <div className="flex items-center">
+          <img
+            src={recipe.frontmatter.author_image}
+            alt="author"
+            className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+          />
+          <h3 className="text-gray-700 font bold">
+            {recipe.frontmatter.author}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 /** https://github.com/vercel/next.js/discussions/12124 */
 import fs from 'fs';
 import path from 'path';
+import Link from 'next/link';
 import matter from 'gray-matter';
 import Layout from '../components/Layout';
+import Recipe from '../components/Recipe';
 
 export default function HomePage({ recipes }) {
   console.log(recipes);
   return (
     <Layout>
-      <h1>ITS ALIVE</h1>
+      <h1 className="text-5xl border-b-4 font-bold">Latest Recipes</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {recipes.map((recipe, index) => (
+          <Recipe key={index} recipe={recipe} />
+        ))}
+      </div>
+      <Link href="/recipes">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Recipes
+        </a>
+      </Link>
     </Layout>
   );
 }
