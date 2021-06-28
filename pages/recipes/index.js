4a9b9e4b0b7e05abc8a44de6427145f40ba0fb5e
Jeff Pottruff <jeffpottruff@gmail.com>
2021-06-28 12:46:24 -0400

    modify recipe index page

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,8 +1,49 @@
+/** https://github.com/vercel/next.js/discussions/12124 */
+import fs from 'fs';
+import path from 'path';
+import Link from 'next/link';
+import matter from 'gray-matter';
 import Layout from '../../components/Layout';
-export default function RecipePage() {
+import Recipe from '../../components/Recipe';
+import { sortMostRecentDate, maxToDisplay } from '../../utils';
+
+export default function RecipePage({ recipes }) {
+  console.log(recipes);
   return (
     <Layout>
-      <h1>Recipes</h1>
+      <h1 className="text-5xl border-b-4 font-bold">Recipes</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {recipes.map((recipe, index) => (
+          <Recipe key={index} recipe={recipe} />
+        ))}
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('recipes'));
+
+  const recipes = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+    const markdownWithMeta = fs.readFileSync(
+      path.join('recipes', filename),
+      'utf-8'
+    );
+
+    /** https://github.com/jonschlinkert/gray-matter */
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      recipes: recipes.sort(sortMostRecentDate),
+    },
+  };
 }
