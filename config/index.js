eaeb530b4f2788c1fcc6db0ba96dc33d17bc50da
Jeff Pottruff <jeffpottruff@gmail.com>
2021-06-30 12:54:57 -0400

    modify max displayed per page

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 /** Maximum number of posts displayed on the _Home Page_ */
 export const MAX_DISPLAY_HOME = 6;
 /** Maximum number of posts displayed for _pagination_  */
-export const MAX_DISPLAY_PER_PAGE = 3;
+export const MAX_DISPLAY_PER_PAGE = 6;
