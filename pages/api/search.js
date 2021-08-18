ef3ad955d6eba29e0ca98a54474352ea1361ecdc
Jeff Pottruff <jeffpottruff@gmail.com>
2021-08-18 12:57:43 -0400

    Adds search recipes for dev env (WIP)

---
+++ pages/api/search.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+export default (req, res) => {
+  let recipes;
+
+  if (process.env.NODE_ENV === 'production') {
+    // TODO - fetch from cache
+  } else {
+    const files = fs.readdirSync(path.join('recipes'));
+
+    recipes = files.map((filename) => {
+      const markdownWithMeta = fs.readFileSync(
+        path.join('recipes', filename),
+        'utf-8'
+      );
+
+      const { data: frontmatter } = matter(markdownWithMeta);
+
+      return {
+        frontmatter,
+      };
+    });
+  }
+
+  const results = recipes.filter(
+    ({ frontmatter: { title, excerpt, category } }) =>
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.q) != -1
+  );
+
+  res.status(200).json(JSON.stringify({ results }));
+};
